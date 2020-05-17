6d0a54507779c342c697b8ec6f9427a0d965ee8f
lee <junsoo3515@gmail.com>
2020-05-17 23:03:02 +0900

    Updates

---
+++ precache-manifest.ff4b4bee257c4fbe2b31e07743ee890d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfaa2dbf991a5df482b9b9a2868adcc8",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "83ee84d3f41a47d05eaa",
+    "url": "/movie_app/static/css/main.9f3c3914.chunk.css"
+  },
+  {
+    "revision": "8feb7df991a530ceaed1",
+    "url": "/movie_app/static/js/2.11f19f93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.11f19f93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83ee84d3f41a47d05eaa",
+    "url": "/movie_app/static/js/main.5bd76546.chunk.js"
+  },
+  {
+    "revision": "77df4e0b724824eca1cd",
+    "url": "/movie_app/static/js/runtime-main.0b3e8895.js"
+  }
+]);
